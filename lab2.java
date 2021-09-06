2ba0c9dff8acf7b91509a9b31273c0955c402da2
molodec1337322 <54796286+molodec1337322@users.noreply.github.com>
2021-09-06 15:51:50 +0300

    Update lab2.java

--- lab2.java
+++ lab2.java
@@ -13,7 +13,6 @@ public class Main
 	        BigInteger bigInteger = BigInteger.valueOf(i);
 	        if(bigInteger.isProbablePrime((int) Math.log(i))){
 	            sum += someArr[i];
-	            System.out.println(someArr[i]);
 	        }
 	    }
 	    
@@ -21,7 +20,19 @@ public class Main
 	}
 	
 	public static void first18thTask(){
-	    int[] someArr = {1, 5, 9, 8, 4, 6, 12, 3, 5};
+	    int[] someArr = {1, 5, 9, 8, 4, 6, 12, 3, 5, 1, 9};
+	    Map<int, int> map = new HashMap<int, int>();
+	    
+	    for(int i = 0; i < someArr.length; i++){
+	        if(map.containsKey(someArr[i])){
+	            map.replace(someArr[i], map.get(someArr[i]) + 1);
+	        }
+	        else{
+	            map.put(someArr[i], 1);
+	        }
+	    }
+	    
+	    
 	}
 	
 	public static void second14thTask(){
