4ea4fca84e42d04ad3920236f2e8dfb7b5e30314
molodec1337322 <54796286+molodec1337322@users.noreply.github.com>
2021-09-06 21:40:47 +0300

    Update lab2.java

--- lab2.java
+++ lab2.java
@@ -1,45 +1,64 @@
 import java.math.*;
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class Main
 {
-	public static void main(String[] args) {
-		first14thTask();
-	}
-	
-	public static void first14thTask(){
-	    int[] someArr = {1, 5, 9, 8, 4, 6, 12, 3, 5};
-	    int sum = 0;
-	    
-	    for(int i = 0; i < someArr.length; i++){
-	        BigInteger bigInteger = BigInteger.valueOf(i);
-	        if(bigInteger.isProbablePrime((int) Math.log(i))){
-	            sum += someArr[i];
-	        }
-	    }
-	    
-	    System.out.println(sum);
-	}
-	
-	public static void first18thTask(){
-	    int[] someArr = {1, 5, 9, 8, 4, 6, 12, 3, 5, 1, 9};
-	    Map<int, int> map = new HashMap<int, int>();
-	    
-	    for(int i = 0; i < someArr.length; i++){
-	        if(map.containsKey(someArr[i])){
-	            map.replace(someArr[i], map.get(someArr[i]) + 1);
-	        }
-	        else{
-	            map.put(someArr[i], 1);
-	        }
-	    }
-	    
-	    
-	}
-	
-	public static void second14thTask(){
-	    
-	}
-	
-	public static void second18thTask(){
-	    
-	}
+    public static void main(String[] args) {
+        first18thTask();
+    }
+
+    public static void first14thTask(){
+        int[] someArr = {1, 5, 9, 8, 4, 6, 12, 3, 5};
+        int sum = 0;
+
+        for(int i = 0; i < someArr.length; i++){
+            BigInteger bigInteger = BigInteger.valueOf(i);
+            if(bigInteger.isProbablePrime((int) Math.log(i))){
+                sum += someArr[i];
+            }
+        }
+
+        System.out.println(sum);
+    }
+
+    public static void first18thTask(){
+        int[] someArr = {1, 5, 9, 8, 4, 6, 12, 3, 5, 1, 9};
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        for(int i = 0; i < someArr.length; i++){
+            if(map.containsKey(someArr[i])){
+                map.replace(someArr[i], map.get(someArr[i]) + 1);
+            }
+            else{
+                map.put(someArr[i], 1);
+            }
+        }
+
+        int minKey = Integer.MAX_VALUE;
+        int maxVal = Integer.MIN_VALUE;
+
+        for(Map.Entry entry: map.entrySet()){
+            if((int)entry.getValue() == maxVal){
+                if((int)entry.getKey() < minKey){
+                    minKey = (int)entry.getKey();
+                }
+            }
+            if((int)entry.getValue() > maxVal){
+                maxVal = (int)entry.getValue();
+                minKey = (int)entry.getKey();
+            }
+        }
+
+        System.out.println(minKey + ": " + maxVal);
+    }
+
+    public static void second14thTask(){
+
+    }
+
+    public static void second18thTask(){
+
+    }
 }
